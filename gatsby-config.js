b9fe79c0451bc79a51cd631be9df7217f7b85f6d
Kat Biel <katarzynaa89@gmail.com>
2024-07-30 12:32:02 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `KatBiel Tech World`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
