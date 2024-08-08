a803d8630156c5b58dcd617fda8d89364dad6551
Kat Biel <kat@rootwire.co.uk>
2024-08-08 16:48:29 +0100

    Commit for deployment nav picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ const IndexPage = () => {
         <p>Where I talk about my experience as a female career changer in a tech industry</p>
         <p>This site is currently under construction... Please, visit back again soon.</p>   
         <div className={styles.imageWrapper}>
-        <StaticImage
+        {/* <StaticImage
         alt="A notebook, ipad and a cup of coffee or tea on a table"
         src="../images/pexels-pixabay-261579.jpg"
-        />
+        /> */}
         </div>
       </Layout>
     </main>
